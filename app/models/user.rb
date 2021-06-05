3e178165d03b3a0c3342e9371cf7b8a1a6ebe3d4
KenInoue1911 <inouetkrk3@gmail.com>
2021-06-05 12:29:49 +0000

    お気に入り機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   # フォロー取得
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   # フォロワー取得
