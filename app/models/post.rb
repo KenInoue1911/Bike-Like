3f1c7b0bafc810737accee5a26a787291dc2195c
KenInoue1911 <ec2-user@ip-172-31-10-211.ap-northeast-1.compute.internal>
2021-06-20 12:12:04 +0000

    6/20

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,10 @@ class Post < ApplicationRecord
   acts_as_taggable
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+  validates :title, {length: {in: 1..15} }
+  validates :post_profile, {length: {in: 1..200} }
+  validates :post_bike, {length: {in: 1..8} }
+  validates :image, presence: true
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name,    length: { in: 1..9 }
+  validates :title,    length: { maximum: 100 }
   mount_uploader :avater, AvaterUploader
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
@@ -30,8 +32,8 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
-  
-  
+
+
   enum gender: {男性: 0, 女性: 1}
   # 退会しているユーザーがログインできないようにする
   def active_for_authentication?
